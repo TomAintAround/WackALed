ed9099da16296eee3a73910ab0fd2ff8047c67ba
TomAintAround <tcorreia2005@proton.me>
2024-10-30 17:19:21 +0000

    chore: setting up the IO ports

--- WackALed.ino
+++ WackALed.ino
@@ -1,4 +1,12 @@
+byte minLed = 7;
+byte maxLed = 13;
+byte buttonPin = 4;
+
 void setup() {
+    Serial.begin(9600);
+
+    for (byte led = minLed; led <= maxLed; led++) pinMode(led, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLUP)
 }
 
 void loop() {
