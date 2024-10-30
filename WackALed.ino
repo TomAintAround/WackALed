36e5e582f5cead3b9e2398e32f3c37cedb48242d
TomAintAround <tcorreia2005@proton.me>
2024-10-30 18:52:33 +0000

    feat: add debouncing protection

--- WackALed.ino
+++ WackALed.ino
@@ -1,6 +1,38 @@
 byte minLed = 7;
 byte maxLed = 13;
 byte buttonPin = 4;
+bool ultimoEstadoBotao = LOW;
+bool estadoBotaoDebouncing = LOW;
+long ultimoDebounce = 0;
+byte delayDebounce = 50;
+
+void debouncing() {
+    /*
+        Contornar o problema do debouncing dos botões
+    */
+    bool estadoBotao = !digitalRead(buttonPin);
+
+    // Se o estado do botão for diferente, atualizar o tempo do último debounce.
+    if (estadoBotao != ultimoEstadoBotao) ultimoDebounce = millis();
+
+    // Executar se o tempo atual menos o do último debounce for maior que a margem definida
+    if (millis - ultimoDebounce > delayDebounce) {
+
+        // Executar se o estado do botão tiver mudado
+        if (estadoBotaoDebouncing != estadoBotao) {
+            estadoBotaoDebouncing = estadoBotao;
+            if (estadoBotaoDebouncing == HIGH) {
+                acaoAtual();
+            }
+        }
+    }
+
+    ultimoEstadoBotao = estadoBotao;
+}
+
+void acaoAtual() {
+
+}
 
 void setup() {
     Serial.begin(9600);
@@ -10,4 +42,5 @@ void setup() {
 }
 
 void loop() {
+    debouncing();
 }
